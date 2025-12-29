e7d157b85df79bc1e5f796395146e7e9975e41ce
Anthony Fairchild <fairchild.anthony@gmail.com>
2025-12-29 15:48:09 -0500

    wip

--- src/dsl.lisp
+++ src/dsl.lisp
@@ -15,6 +15,9 @@
 (defmethod render ((element string))
   (format t "~a~%" element))
 
+(defmethod render ((element function))
+  (funcall element))
+
 (defparameter *choices* nil)
 
 (defmethod perform ((scene scene))
@@ -56,21 +59,21 @@
 								  (setf *current-scene* south-scene))))))
     (setf north-scene
 	  (make-instance 'scene :name 'north
-			 :elements (list "You are in a bright room."
-					 (make-instance 'choice
-							:description "Go back south."
-							:action (lambda ()
-								  (format t "You go back south.~%")
-								  (setf *current-scene* start-scene))))))
+				:elements (list "You are in a bright room."
+						(make-instance 'choice
+							       :description "Go back south."
+							       :action (lambda ()
+									 (format t "You go back south.~%")
+									 (setf *current-scene* start-scene))))))
     (setf south-scene
 	  (make-instance 'scene :name 'south
-			 :elements (list "You are in a gloomy cave."
-					 (make-instance 'choice
-							:description "Go back north."
-							:action (lambda ()
-								  (format t "You go back north.~%")
-								  (setf *current-scene* start-scene))))))
-	(setf *current-scene* start-scene)
+				:elements (list "You are in a gloomy cave."
+						(make-instance 'choice
+							       :description "Go back north."
+							       :action (lambda ()
+									 (format t "You go back north.~%")
+									 (setf *current-scene* start-scene))))))
+    (setf *current-scene* start-scene)
     (play-game start-scene)))
 
 
@@ -107,10 +110,39 @@
 	     (goto start))
       ("no"
        (text "Final count is " count)))
-    ))1
+    ))
 
 
-#+nil(defun run-game ()
+(eval-when (:load-toplevel :compile-toplevel :execute)
+  (defmacro game (&body body)
+    `(make-instance 'scene
+		    :name 'main
+		    :elements (list ,@body)))
+  
+  (defmacro text (&rest args)
+    `(lambda ()
+       (format t ,(apply #'concatenate 'string (mapcar (lambda (x)
+							 (if (stringp x)
+							     x
+							     "~a"))
+						       args))
+	       ,@(remove-if #'stringp args))))
+
+  (defmacro choice (description &body body)
+    `(progn
+       (format t "~A~%" ,description)
+       ,@(loop for item in body
+	       collect  (let ((desc (car item))
+			      (action-body (cdr item)))
+			  `(make-instance 'choice
+					  :description ,desc
+					  :action (lambda ()
+						    @,(loop for elem in action-body
+							    collect `(render-element ,elem))))))))
+  )
+
+
+(defun run-game ()
   (game 
    (text "Your majesty, your people are starving in the streets, and threaten revolution.
 Our enemies to the west are weak, but they threaten soon to invade.  What will you do?")
