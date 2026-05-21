56dee8b03b541a552c53fdc836b86fc93ecb7d70
Anthony Fairchild <fairchild.anthony@gmail.com>
2026-05-20 22:34:32 -0400

    Add user and examples packages

---
+++ examples/package.lisp
@@ -0,0 +1,4 @@
+(uiop:define-package #:dunge-examples
+  (:use #:cl #:dunge)
+  (:shadowing-import-from #:dunge #:room)
+  (:export #:basic-example))
--- src/package.lisp
+++ src/package.lisp
@@ -2,4 +2,48 @@
 (uiop:define-package #:dunge
   (:use #:cl)
   (:mix #:trivia)
-  (:shadow #:room))
+  (:shadow #:room)
+  (:export
+   #:*input*
+   #:*output*
+   #:back
+   #:choice
+   #:close-choice
+   #:collect-choices
+   #:container
+   #:container-view
+   #:contents
+   #:describe-entity
+   #:description
+   #:enter
+   #:enter-target
+   #:entities
+   #:evaluate
+   #:game
+   #:game-player
+   #:game-rooms
+   #:game-start
+   #:gosub
+   #:goto
+   #:interaction-label
+   #:interaction-target
+   #:item
+   #:label
+   #:name
+   #:open-choice
+   #:option
+   #:options
+   #:p
+   #:placed
+   #:placed-thing
+   #:placement-description
+   #:quit
+   #:room
+   #:room-name
+   #:target
+   #:text
+   #:viewed-container))
+
+(uiop:define-package #:dunge-user
+  (:use #:cl #:dunge)
+  (:shadowing-import-from #:dunge #:room))
